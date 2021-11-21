276953bd997a7387f21c54fc6c021e0a2bbdaa50
Saim Ahmad <zasaimster@gmail.com>
2021-11-20 19:02:06 -0800

    added models

---
+++ backend/models/preferences.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const {String} = Schema.Types;
+
+const PreferencesSchema = new Schema({
+  whoToDate: {
+    // guys, girls, non-binary, etc
+    type: String,
+    default: "Any",
+  },
+  year: {
+    type: String,
+    default: "Any",
+  },
+  major: {
+    type: String,
+    default: "Any",
+  },
+  location: {
+    type: String,
+    default: "Any",
+  },
+  drink: {
+    type: String,
+    default: "Any",
+  },
+  smoke: {
+    type: String,
+    default: "Any",
+  },
+});
+
+const Preferences = mongoose.model("attributes", PreferencesSchema);
+module.exports = Preferences;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,22 +1,100 @@
 const mongoose = require("mongoose");
+const Preferences = require("./preferences.model");
 const Schema = mongoose.Schema;
 
-const { String } = Schema.Types;
+const {String, Array} = Schema.Types;
+
+/**
+ * User model:
+        Id 
+        first name - required
+        Email - required
+        password - required
+        instagram - optional
+        snap - optional,
+        dob
+        attributes (separate object)
+        Preferences (separate object)
+        likedBy 
+        dislikedBy 
+        matches
+ */
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "Name cannot be blank"],
-    },
-    email: {
-        type: String,
-        index: { unique: true },
-        required: [true, "Email cannot be blank"],
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  firstName: {
+    type: String,
+    required: [true, "First name cannot be blank"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name cannot be blank"],
+  },
+  email: {
+    type: String,
+    index: {unique: true},
+    required: [true, "Email cannot be blank"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password cannot be blank"],
+  },
+  dateOfBirth: {
+    type: Date,
+    required: [true, "Birthday cannot be blank"],
+  },
+  gender: {
+    type: String,
+    required: [true, "Gender cannot be blank"],
+  },
+  ethnicity: {
+    type: String,
+    required: [true, "Ethnicity cannot be blank"],
+  },
+  height: {
+    type: String,
+    required: [true, "Height cannot be blank"],
+  },
+  year: {
+    type: String,
+    required: [true, "Year cannot be blank"],
+  },
+  location: {
+    type: String,
+    required: [true, "Location cannot be blank"],
+  },
+  major: {
+    type: String,
+    required: [true, "Major cannot be blank"],
+  },
+  smoke: {
+    type: String,
+    required: [true, "Smoking cannot be blank"],
+  },
+  drink: {
+    type: String,
+    required: [true, "Drink cannot be blank"],
+  },
+  preferences: Preferences,
+  instagram: {
+    type: String,
+    default: "",
+  },
+  snapchat: {
+    type: String,
+    default: "",
+  },
+  likedBy: {
+    type: Array,
+    default: [],
+  },
+  dislikedBy: {
+    type: Array,
+    default: [],
+  },
+  matches: {
+    type: Array,
+    default: [],
+  },
 });
 
 const User = mongoose.model("user", UserSchema);
