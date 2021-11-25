4f9954319eb3ec28ca5d5d1d1cd9c54e320d8544
Chang <32664916+ChangXord@users.noreply.github.com>
2021-11-25 14:40:14 -0800

    connected to server end; updated main page

--- frontend/lib/common/widgets/card.dart
+++ frontend/lib/common/widgets/card.dart
@@ -2,7 +2,10 @@ import 'package:bdate/common/values/values.dart';
 import 'package:flutter/material.dart';
 import 'package:bdate/common/entity/entity.dart';
 import 'package:bdate/common/utils/utils.dart';
-Widget card(String first, String last, String url) {
+Widget card(User? user) {
+  String smoke = user?.smoke ?? false ? "Smokes" : "Doesn't Smoke";
+  String drink = user?.drink ?? false ? "Drinks" : "Doesn't Drink";
+  String jb = "https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/256226769_306626027952781_4469855532132233091_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lV4tn-5rt6gAX_7NwGl&_nc_ht=scontent-lax3-1.xx&oh=936888099a717f23d920bebe709c05ea&oe=61A50B98";
   return Container(
     margin: EdgeInsets.only(top: h(60)),
     width: w(267),
@@ -10,16 +13,28 @@ Widget card(String first, String last, String url) {
     child: Stack(
       fit: StackFit.expand,
       children: <Widget>[
-        ClipRRect(borderRadius: BorderRadius.all(Radius.circular(15)), child: Image.network(url, fit: BoxFit.fitHeight,)),   
+        ClipRRect(borderRadius: BorderRadius.all(Radius.circular(15)), child: Image.network("${user?.photo ?? jb}", fit: BoxFit.fitHeight,)),   
         SingleChildScrollView(
           
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               SizedBox(height: h(435)),
-              Container(child: Text(first+" "+last, style: TextStyle(color: AppColors.primaryElementText, fontSize: f(32), fontWeight: FontWeight.w500, shadows: [Shadow(offset: Offset(2,2), color: Colors.grey, blurRadius: 1)] ),), margin: EdgeInsets.only(left: h(13)),),
+              Container(
+                child: Text(
+                  "${user?.firstName ?? "Joe"} ${user?.lastName ?? "Bruin"} ", 
+                  style: TextStyle(color: AppColors.primaryElementText, fontSize: f(32), fontWeight: FontWeight.w500, shadows: [Shadow(offset: Offset(2,2), color: Colors.grey, blurRadius: 1)] ),
+                ), 
+                margin: EdgeInsets.only(left: h(13)),
+              ),
               SizedBox(height: h(83)),
-              Container(child: Text("DOB\nEth", style: TextStyle(color: AppColors.primaryElementText, fontSize: f(18)),), margin: EdgeInsets.only(left: h(24)),),
+              Container(
+                child: Text(
+                  "${user?.age ?? 97}\n${user?.gender ?? "female"}\n${user?.ethnicity ?? "bear"}\n${user?.height ?? "mystery"}\n${user?.year ?? "have a guess"}\n${user?.location ?? "anywhere in UCLA"}\n${user?.major ?? "fun"}\n$smoke\n$drink\n", 
+                  style: TextStyle(color: AppColors.primaryElementText, fontSize: f(18), fontWeight: FontWeight.w500, shadows: [Shadow(offset: Offset(2,2), color: Colors.grey, blurRadius: 1)]),
+                  ), 
+                margin: EdgeInsets.only(left: h(24)),
+              ),
             ],
           ),
         )
--- frontend/lib/pages/discover.dart
+++ frontend/lib/pages/discover.dart
@@ -1,3 +1,5 @@
+import 'package:bdate/common/api/get_user.dart';
+import 'package:bdate/common/entity/user.dart';
 import 'package:bdate/common/utils/utils.dart';
 import 'package:bdate/common/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -9,22 +11,25 @@ class discoverPage extends StatefulWidget {
 }
 
 class _discoverPageState extends State<discoverPage> {
-  late String first;
-  late String last;
-  late String photo;
+
+  User? user;
+
   @override
   void initState() {
+    _loadUser().whenComplete((){setState(() {});});
     super.initState();
-    first = "first";
-    last = "last";
-    photo = "https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg";
+
+  }
+
+  _loadUser() async {
+    user = await GetUser.getUser();
+    if(mounted){
+      setState(() {});
+    }
   }
 
   VoidCallback handleLike() {
     return () => setState(() {
-      first = "Joe";
-      last = "Bruin";
-      photo = "https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/256226769_306626027952781_4469855532132233091_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=jsco4hu_vuQAX93DMQ6&_nc_ht=scontent-lax3-1.xx&oh=99e25287c426395415b6c2b629e6c183&oe=61A11718";
     });
   }
 
@@ -35,7 +40,7 @@ class _discoverPageState extends State<discoverPage> {
         child: Column(
           children: <Widget>[
             // _buildLogo(),
-            card(first, last, photo),
+            card(user),
             Container(
               margin: EdgeInsets.only(top: h(15)),
               width: h(231),
