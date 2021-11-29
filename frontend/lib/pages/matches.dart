1e68917ff3d3dd54eb755d64cd7b8d9ff8bf8b90
Darren Wu <darren@Darrens-MacBook-Pro.local>
2021-11-29 00:55:45 -0800

    Added Title to Matches Page

--- frontend/lib/pages/matches.dart
+++ frontend/lib/pages/matches.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:bdate/common/utils/utils.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class matchesPage extends StatefulWidget {
-  const matchesPage({ Key? key }) : super(key: key);
+class matchesPage extends StatelessWidget {
+  matchesPage({Key? key}) : super(key: key);
 
-  @override
-  _matchesPageState createState() => _matchesPageState();
-}
-
-class _matchesPageState extends State<matchesPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    ScreenUtil.init(
+      BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width,
+          maxHeight: MediaQuery.of(context).size.height),
+      designSize: Size(375, 812),
+    );
+    return Scaffold(
+      body: Center(
+        child: Container(
+            child: Column(
+          children: <Widget>[
+            _title(),
+          ],
+        )),
+      ),
     );
   }
+}
+
+Widget _title() {
+  return Container(
+      margin: EdgeInsets.only(top: h(50)),
+      child: const Text("Matches",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: "Montserrat",
+            fontWeight: FontWeight.w600,
+            fontSize: 30,
+            height: 1,
+          )));
 }
